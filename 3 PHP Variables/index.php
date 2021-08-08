ef393205faa6ae8e10100e9700e6e55f4e1188f9
Rahul Golani <rahulgolani24@live.com>
2021-08-08 22:35:15 +0530

    PHP Variables

---
+++ 3 PHP Variables/index.php
@@ -0,0 +1,18 @@
+<?php
+// Keywords in php are NOT case sensitive
+// Variables in php are case sensitive
+
+// -----------------Variables---------------
+
+// starts with a $
+
+$name="Rahul Golani";
+$profession="Software Engineer";
+
+// PHP is a loosely typed language and it can automatically assign the type to the variable 
+
+echo "My name is $name <br>"; //br is to be used since the code will be evaluated into html code
+echo "I am a $profession <br>";
+
+
+?>
