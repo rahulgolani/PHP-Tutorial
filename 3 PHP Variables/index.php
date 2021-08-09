38ca7d5df6627c04803a188efa1cba1e7cd84aa8
Rahul Golani <rahulgolani24@live.com>
2021-08-10 00:08:51 +0530

    More on Variables

--- 3 PHP Variables/index.php
+++ 3 PHP Variables/index.php
@@ -1,5 +1,5 @@
 <?php
-// Keywords in php are NOT case sensitive
+// Keywords in php are NOT case sensitive - echo and ECHO are same
 // Variables in php are case sensitive
 
 // -----------------Variables---------------
@@ -14,5 +14,13 @@ $profession="Software Engineer";
 echo "My name is $name <br>"; //br is to be used since the code will be evaluated into html code
 echo "I am a $profession <br>";
 
+
+// Variable Rules-> 
+
+// 1) Starts with a $
+// 2) Cannot start with number
+// 3) Must start with character or a underscore
+// 4) Can only contain alphanumeric characters and number
+// 5) Variable names are case sensitive
 
 ?>
