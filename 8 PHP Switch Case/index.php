3361384cecd66db7c6956ed81ed0d2d4e5141d1e
Rahul Golani <rahulgolani24@live.com>
2021-08-15 19:17:26 +0530

    Switch Statements

---
+++ 8 PHP Switch Case/index.php
@@ -0,0 +1,22 @@
+<?php
+
+$age=22;
+
+switch ($age){
+    case 12:
+        echo "Your Age is 12";
+        break;
+    case 24:
+        echo "Your age is 24";
+        break;
+    case 36:
+        echo "Your age is 36";
+        break;
+    default:
+        echo "Age is not defined";
+    
+}
+
+
+
+?>
