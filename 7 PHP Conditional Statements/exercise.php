5a29373038dff9774019bc5cb141ecceec138dae
Rahul Golani <rahulgolani24@live.com>
2021-08-15 19:08:25 +0530

    Conditional Statements

---
+++ 7 PHP Conditional Statements/exercise.php
@@ -0,0 +1,15 @@
+<?php
+
+$age=25;
+
+if ($age>65){
+    echo "You cannot drive";
+}
+elseif ($age>=25 and $age<65){
+    echo "You can drive";
+}
+else{
+    echo "You cannot drive";
+}
+
+?>
---
+++ 7 PHP Conditional Statements/index.php
@@ -0,0 +1,25 @@
+<?php
+
+$a=10;
+$b=5;
+
+if ($a>10){
+    echo "a is greater than 10 <br>";
+}
+else{
+    echo "a is not greater than 10 <br>";
+}
+
+$age=18;
+
+if ($age>18){
+    echo "You can hold a driving license <br>";
+}
+elseif($age==18){
+    echo "You can hold a learning driving license <br>";
+}
+else{
+    echo "You cannot hold a driving license <br>";
+}
+
+?>
