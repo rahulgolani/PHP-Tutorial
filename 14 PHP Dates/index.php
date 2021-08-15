c5f879dea7b9ff0b84803001fb04b7def5d4e112
Rahul Golani <rahulgolani24@live.com>
2021-08-15 21:40:29 +0530

    Dates

---
+++ 14 PHP Dates/index.php
@@ -0,0 +1,19 @@
+<?php
+
+// date_default_timezone_set('GMT');
+
+$todaysDate=date("dS F Y ");
+echo $todaysDate, "<br>";
+
+
+$todaysDate=date("dS F Y, h:i:s A");
+echo $todaysDate, "<br>";
+
+
+// $todaysDate=date("dS F Y ");
+// echo $todaysDate;
+echo "<br><br><br><br><br><br>";
+
+$year=date("Y");
+echo "Copyright $year. All Rights Reserved";
+?>
