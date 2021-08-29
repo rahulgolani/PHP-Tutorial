e2b9fc82b2ab0e3f88bbcbf4ea723db01f14bac1
Rahul Golani <rahulgolani24@live.com>
2021-08-29 15:37:43 +0530

    PHP - Working with Files

---
+++ 29 PHP Working with Files/index.php
@@ -0,0 +1,10 @@
+<?php
+
+//readfile prints the content of the file and returns the number of characters read
+// $a=readfile("myFile.txt");
+
+readfile("myFile.txt");
+
+//Can also be used if we are to render any html template
+
+?>
